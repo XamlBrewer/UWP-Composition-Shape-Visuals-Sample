0cd8d7cfe4188641016b08c4a8c6aac0116fd4a3
Diederik Krols <diederik_krols@hotmail.com>
2018-07-09 15:23:05 +0200

    Compositor extension method

---
+++ XamlBrewer.Uwp.Composition.ShapesSample/Controls/CompositorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CompositionProToolkit.Win2d;
+using Microsoft.Graphics.Canvas;
+using Windows.UI.Composition;
+
+namespace XamlBrewer.Uwp.Controls
+{
+    public static class CompositorExtensions
+    {
+        public static CompositionSpriteShape CreateSpriteShape(this Compositor compositor, string path)
+        {
+            var canvasGeometry = CanvasObject.CreateGeometry(new CanvasDevice(), path);
+            var compositionPath = new CompositionPath(canvasGeometry);
+            var pathGeometry = compositor.CreatePathGeometry(compositionPath);
+
+            return compositor.CreateSpriteShape(pathGeometry);
+        }
+    }
+}
